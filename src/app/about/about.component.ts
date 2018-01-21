267196496879c07daa1d9e178012e7d2fa9c4c24
Thomas Coleman <tom@ilikeprograms.com>
2018-01-21 13:06:54 +0000

    Added about info

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-about-component',
   template: `
     <h2>About</h2>
+    <p>Porthole is a tools for Digital marketers which makes it easier to manage and change keywords, suitable for use within AdWords.</p>
+    <p>If you have found Porthole useful, please share it on Social media and help us out!</p>
   `
 })
 export class AboutComponent {}
