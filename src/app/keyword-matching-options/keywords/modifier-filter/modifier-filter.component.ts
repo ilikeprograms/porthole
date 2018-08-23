0a38ecd450cc4b28d4d1db01ef23ad8856c3ef5f
Thomas Coleman <tom@ilikeprograms.com>
2018-08-24 00:16:39 +0100

    Updated to make the keywords table filter modifiers with checkboxes instead of select field, so now supporting multiple filters

--- src/app/keyword-matching-options/keywords/modifier-filter/modifier-filter.component.ts
+++ src/app/keyword-matching-options/keywords/modifier-filter/modifier-filter.component.ts
@@ -1,33 +1,34 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+
 import { ClrDatagridFilterInterface } from '@clr/angular';
 import { BehaviorSubject, Subject } from 'rxjs';
+
 import { IKeyword } from '../keyword.interface';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { takeUntil } from 'rxjs/operators';
 import { KeywordModifiers } from '../keyword-modifier-enum';
-
-interface ISelectedModifierFormValues {
-  selectedModifier: string;
-}
 
 @Component({
   selector: 'app-modifier-filter',
   templateUrl: './modifier-filter.component.html'
 })
-export class ModifierFilterComponent implements ClrDatagridFilterInterface<IKeyword> {
+export class ModifierFilterComponent implements OnInit, OnDestroy, ClrDatagridFilterInterface<IKeyword> {
   public filterForm: FormGroup;
+
+  public keywordModifiers: any = KeywordModifiers;
 
   private ngUnsubscribe$: Subject<void> = new Subject<void>();
 
-  private currentFilter$: BehaviorSubject<KeywordModifiers | string> = new BehaviorSubject('');
+  private currentFilter$: BehaviorSubject<Array<boolean>> = new BehaviorSubject([]);
 
   constructor() {
     this.setupForm();
   }
 
   public ngOnInit(): void {
-    this.filterForm.valueChanges.pipe(takeUntil(this.ngUnsubscribe$)).subscribe((values: ISelectedModifierFormValues) => {
-      this.currentFilter$.next(values.selectedModifier);
+    this.filterForm.controls.selectedModifier.valueChanges.pipe(takeUntil(this.ngUnsubscribe$)).subscribe((values: Array<boolean>) => {
+      this.currentFilter$.next(values);
+
       this.changes.emit(true);
     });
   }
@@ -40,16 +41,28 @@ export class ModifierFilterComponent implements ClrDatagridFilterInterface<IKeyw
   changes: EventEmitter<boolean> = new EventEmitter<any>(false);
 
   isActive(): boolean {
-    return this.currentFilter$.getValue() !== '';
+    const selectedModifiers: Array<boolean> = this.currentFilter$.getValue();
+
+    return selectedModifiers.some((isSelected: boolean) => {
+      return isSelected;
+    });
   }
 
   accepts(keyword: IKeyword): boolean {
-    return keyword.modifier === this.currentFilter$.getValue();
+    const selectedModifiers: Array<boolean> = this.currentFilter$.getValue();
+
+    return selectedModifiers[keyword.modifier];
   }
 
   private setupForm(): void {
     this.filterForm = new FormGroup({
-      selectedModifier: new FormControl('', Validators.required)
+      selectedModifier: new FormArray([
+        new FormControl(false),
+        new FormControl(false),
+        new FormControl(false),
+        new FormControl(false),
+        new FormControl(false),
+      ])
     });
   }
 }
